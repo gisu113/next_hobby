8246cf34c129ec7319abea73f02f32da7449911c
gisu113 <gisan133555@gmail.com>
2023-08-20 20:18:46 +0900

    テーブルの作成

--- db/migrate/20230820110207_devise_create_users.rb
+++ db/migrate/20230820110207_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,        null: false, default: ""
+      t.integer :gender
+      t.date   :date_of_birth
+      t.integer :prefecture_id
 
       ## Recoverable
       t.string   :reset_password_token
