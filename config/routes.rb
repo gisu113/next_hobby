c0da2c732781438cca4b31c29afa38780984679f
gisu113 <gisan133555@gmail.com>
2023-08-22 00:24:54 +0900

    新規投稿ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
