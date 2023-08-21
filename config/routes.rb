759f74a4a0bdeb62366533232a72d7467cad4719
gisu113 <gisan133555@gmail.com>
2023-08-22 02:22:06 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'posts#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
