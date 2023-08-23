8de61ec0333f04d48a04eeb62efc68a5c644d561
gisu113 <gisan133555@gmail.com>
2023-08-23 22:44:49 +0900

    トップページの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'posts#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
 end
