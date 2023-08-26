c31c59093577b24ebb70e26c9bf22244d75d50e0
gisu113 <gisan133555@gmail.com>
2023-08-27 05:42:53 +0900

    登録表記の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,8 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def user_params
+    params.require(:user).permit(:nickname, :email,)
+  end
+
 end
--- db/migrate/20230821083409_create_posts.rb
+++ db/migrate/20230821083409_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :hobby
-      t.integer :required_time
-      t.integer :budget
-      t.integer :number_of_people
+      t.string :hobby,             null: false, default: ""
+      t.integer :required_time,     null: false, default: ""
+      t.integer :budget,            null: false, default: ""
+      t.integer :number_of_people,  null: false, default: ""
       t.timestamps
     end
   end
