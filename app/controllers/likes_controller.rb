eda08b05c8840c2cf5473367a3ff902059a7fab4
gisu113 <gisan133555@gmail.com>
2023-09-06 11:15:00 +0900

    ログアウトユーザーがいいねを押せないように変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,18 @@
 class LikesController < ApplicationController
   before_action :set_post
+  before_action :authenticate_user!
 
   def create
     like = current_user.likes.build(post_id: params[:post_id])
-    like.save
-    respond_to do |format|
-      format.js
+    if like.save
+      respond_to do |format|
+        format.js
+      end
+    else
+      # いいねが正常に保存されなかった場合のエラーハンドリングを行う
+      # 例: エラーメッセージを設定し、適切なリダイレクトを行う
+      flash[:alert] = 'いいねの作成に失敗しました。'
+      redirect_to root_path
     end
   end
 
